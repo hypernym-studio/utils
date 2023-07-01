018c15e42e0bddca19aef832c5b596617912bb43
Ivo Dolenc <51852491+ivodolenc@users.noreply.github.com>
2023-07-01 20:13:14 +0200

    feat: stable release (#6)

--- src/generic.ts
+++
@@ -1,5 +0,0 @@
-/**
- * An empty arrow function that performs no operation.
- */
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export const noop = () => {}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export * from './generic'
+export * from './base'
 export * from './is'
