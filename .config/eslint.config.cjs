009c36bd812a8caadeffaf9ff5e617ae6729b834
Ivo Dolenc <ivodolenc@gmail.com>
2023-09-19 12:55:41 +0200

    chore: update dev configs

--- .config/eslint.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['@hypernym/eslint-config'],
-
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-empty-function': 'off'
-  }
-}
