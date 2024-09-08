35ac51f2e334d95ad0db23e077f75e247bdbd147
Ivo Dolenc <ivodolenc@gmail.com>
2024-09-08 13:12:28 +0200

    chore: update bundler config

--- bundler.config.ts
+++ bundler.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@hypernym/bundler'
 
 export default defineConfig({
+  alias: true,
   entries: [
     { input: './src/index.ts' },
     { types: './src/types/index.ts' },
