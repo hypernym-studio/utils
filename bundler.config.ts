f43049df6b27f1ed70d567fc3d28360dc7e9ec0e
Ivo Dolenc <ivodolenc@gmail.com>
2024-09-18 17:13:40 +0200

    chore: update main bundler config

--- bundler.config.ts
+++ bundler.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from '@hypernym/bundler'
+import { defineConfig, replacePath } from '@hypernym/bundler'
 
 export default defineConfig({
-  alias: true,
   entries: [
     { input: './src/index.ts' },
-    { types: './src/types/index.ts' },
-    { input: './src/fs/index.ts' },
-    { types: './src/types/fs/index.ts' },
+    { declaration: './src/types/index.ts' },
+    {
+      input: './src/fs/index.ts',
+      externals: [/^node/, /^@/],
+      paths: (id) => replacePath(/^@/, '../index.mjs')(id),
+    },
+    { declaration: './src/types/fs/index.ts' },
   ],
 })
