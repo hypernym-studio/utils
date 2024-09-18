56a7e0431b11324892ac1a90f6ac2c3a8b5c3291
Ivo Dolenc <ivodolenc@gmail.com>
2024-09-18 20:48:54 +0200

    refactor: update fs write util

--- src/fs/index.ts
+++ src/fs/index.ts
@@ -1,4 +1,4 @@
 export * from './exists'
-export * from './write-file'
+export * from './write'
 export * from './copy'
 export * from './mkdir'
---
+++ src/fs/write.ts
@@ -0,0 +1,34 @@
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { mkdir, writeFile as writefile } from 'node:fs/promises'
+import { isURL } from '@'
+
+export type WritePath = string | URL
+
+/**
+ * Writes data to a file recursively.
+ *
+ * @example
+ *
+ * ```ts
+ * import { write } from '@hypernym/utils/fs'
+ *
+ * await write('dir/subdir/file.ts', `console.log('Hello World!')`)
+ * ```
+ */
+export async function write(
+  path: WritePath,
+  data: Parameters<typeof writefile>[1],
+  options?: Parameters<typeof writefile>[2],
+): Promise<void> {
+  await mkdir(dirname(isURL(path) ? fileURLToPath(path) : path), {
+    recursive: true,
+  })
+
+  await writefile(path, data, options)
+}
+
+/**
+ * @deprecated Use `write` instead.
+ */
+export const writeFile = write
