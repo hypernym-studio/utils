077d70962f7c6cb0b77f7202c0631b3857c1f1de
Ivo Dolenc <ivodolenc@gmail.com>
2024-09-19 13:33:33 +0200

    chore: add new module formats

--- bundler.config.ts
+++ bundler.config.ts
@@ -3,6 +3,25 @@ import { defineConfig, replacePath } from '@hypernym/bundler'
 export default defineConfig({
   entries: [
     { input: './src/index.ts' },
+    {
+      input: './src/index.ts',
+      output: './dist/index.min.mjs',
+      transformers: { esbuild: { minify: true } },
+    },
+    {
+      input: './src/index.ts',
+      output: './dist/index.iife.js',
+      transformers: { esbuild: { minify: true } },
+      format: 'iife',
+      name: 'Hyperutils',
+    },
+    {
+      input: './src/index.ts',
+      output: './dist/index.umd.js',
+      transformers: { esbuild: { minify: true } },
+      format: 'umd',
+      name: 'Hyperutils',
+    },
     { declaration: './src/types/index.ts' },
     {
       input: './src/fs/index.ts',
