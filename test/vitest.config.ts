92a9832babd88c8539142487d820e552f0c3c3d2
Ivo Dolenc <ivodolenc@gmail.com>
2025-02-16 14:42:36 +0100

    test: update test dir

---
+++ test/vitest.config.ts
@@ -0,0 +1,13 @@
+import { cwd } from 'node:process'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: { '@': resolve(cwd(), './src') },
+  },
+  test: {
+    include: ['test/**/*.test.ts'],
+    reporters: ['verbose'],
+  },
+})
